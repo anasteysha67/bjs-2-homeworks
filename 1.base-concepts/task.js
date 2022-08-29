214ea89255e8241a7d2d37f060bb0efdbab0c13b
anasteysha67 <111174272+anasteysha67@users.noreply.github.com>
2022-08-29 18:52:03 +0500

    Added the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict";
+function solveEquation(a, b, c)  {
+  let arr = [];
+  let x1, x2;
+  let D = b ** 2 - 4 * a * c;
+
+  if (D === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    arr.push(x1);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x2);
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
