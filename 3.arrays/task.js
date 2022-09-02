bf5863746de3dcd12309f71b34cfa33b43de4d12
anasteysha67 <111174272+anasteysha67@users.noreply.github.com>
2022-09-02 21:41:23 +0500

    added homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
+//Задание 1
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
-
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+    let result;
+    result = arr1.every((item, i) => item === arr2[i] && arr1.length === arr2.length);
+  
+    return result; // boolean
+  }
+  
+  //Задание 2
+  function advancedFilter(arr) {
+    let resultArr;
+  
+    resultArr = arr.filter(arr => (arr > 0) && (arr % 3 ===0)).map(arr => arr * 10);
+    
+    return resultArr; // array
+  }
