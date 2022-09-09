9ff37a6428ec65b690ce776477c604cb26240605
anasteysha67 <111174272+anasteysha67@users.noreply.github.com>
2022-09-09 10:41:32 +0500

    added homework

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,63 @@
+//задача 1
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+    function wrapper(...args) {
+      const hash = args.join(','); 
+      let objectInCache = cache.find((item) => item == hash); 
+      let result = cache[objectInCache];
+      if (!objectInCache) { 
+        cache.push(hash) ; 
+        cache[hash] = result = func.call(this, ...args);
+        if (cache.length > 5) { 
+         cache.splice(0,1) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;   
+      } else {
+        console.log("Из кэша: " + result); 
+        return "Из кэша: " + result; 
+    }
+  }
+  return wrapper;
+  }
 
+//задача 2
+  function debounceDecoratorNew(func, ms) {
+    let timerId;
+    function wrapperSend(...args){
+      clearTimeout(timerId);
+      if(wrapperSend.history === undefined){
+          wrapperSend.history = [1];
+          setTimeout(() => {
+            return func.call()
+          },0) 
+      }
+      else {
+          timerId = setTimeout(() => {
+            return func.call()
+          },ms)
+      }
+    }
+    return wrapperSend;
+  }
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+ //задача 3 
+function debounceDecorator2(func, ms) {
+    let timerId;
+    function wrapperSend(...args){
+        clearTimeout(timerId);
+        if(wrapperSend.count === undefined){
+            wrapperSend.count = 1;
+            setTimeout(() => {
+                return func.call()
+            },0) 
+            }
+        else {
+            wrapperSend.count ++;
+            timerId = setTimeout(() => {
+                return func.call()
+            },ms)
+        }
+    }
+    return wrapperSend;
 }
